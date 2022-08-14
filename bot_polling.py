ed1bf4c40c06b95094588f2bd63036bc5bfc8aea
theATH <73857020+theatho@users.noreply.github.com>
2022-08-14 21:03:53 +0300

    Add files via upload

---
+++ bot_polling.py
@@ -0,0 +1,31 @@
+## ها خير عاي شنو تدور ؟ 
+
+import json
+import requests
+from requests import get,post
+from system import Bot
+from config import *
+
+tttsel = getenv("BOT_TOKEN")
+api = f"https://api.telegram.org/bot" + tttsel + "/"
+update_id = 0
+
+print("تم تشغيل البوت BY @SELVER7")
+print("PRESS CTRL + C TO EXIT ")
+while True:
+	try:
+		req = get(f"https://api.telegram.org/bot{tttsel}/getupdates",params={"offset":update_id}).json()
+		if len(req['result']) == 0:
+			continue
+		try:
+			update = req["result"][0]
+#			for update in update:
+			Bot(update)
+			update_id = update['update_id'] + 1
+			print("-"*40)
+		except KeyError:
+			continue
+	except KeyboardInterrupt:
+		exit()
+	except requests.exceptions.ConnectionError:
+		continue
